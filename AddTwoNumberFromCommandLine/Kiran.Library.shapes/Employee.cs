cb4593d3bde70b1ed03d758ced4d5960ed5a3d97
Kirankumarvg <kirankumar.vg@gmail.com>
2022-05-06 07:37:15 -0700

    Add files via upload

---
+++ AddTwoNumberFromCommandLine/Kiran.Library.shapes/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiran.Library.shapes
+{
+    public class Employee
+    {
+        string _FirstName;
+        string _LastName;
+        float _salary;
+        public Employee(string firstName )
+        {
+            this._FirstName = firstName;
+            //this._LastName =  
+        }
+
+        //public Employee(string firstName)
+        public Employee(int salary)
+        {
+            this._salary = salary;
+        }
+
+        public float DoIncriment()
+        {
+            float myIncriment;
+            myIncriment = 10 / 100 * _salary;
+            return myIncriment;
+        }
+    }
+}
---
+++ AddTwoNumberFromCommandLine/Kiran.Library.shapes/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiran.Library.shapes
+{
+    public class Reactangle
+    {
+        int lenght;
+        int Breadth;
+
+        
+        public Reactangle(int lenght, int breadth)
+        {
+            //lenght = ;
+        }
+        public int GetReactangle()
+        {
+
+           int ReactangleArea = lenght * Breadth;
+            return ReactangleArea;
+        }
+
+
+    }
+}
